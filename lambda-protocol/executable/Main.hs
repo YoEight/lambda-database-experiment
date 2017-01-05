2b0790d9b3715f2d19b9ccf31528d4fb8f12ae14
YoEight <yo.eight@gmail.com>
2017-01-05 14:33:09 +0100

    Delete useless protocol files

--- lambda-protocol/executable/Main.hs
+++
@@ -1,7 +0,0 @@
--- It is generally a good idea to keep all your business logic in your library
--- and only use it in the executable. Doing so allows others to use what you
--- wrote in their libraries.
-import qualified Example
-
-main :: IO ()
-main = Example.main
