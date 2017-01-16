ce60a95b42aa388871c1966d2403649ba179a5e5
YoEight <yo.eight@gmail.com>
2017-01-16 22:37:44 +0100

    [client] Discard useless files

--- lambda-client/executable/Main.hs
+++ lambda-client/executable/Main.hs
@@ -1,7 +1,6 @@
 -- It is generally a good idea to keep all your business logic in your library
 -- and only use it in the executable. Doing so allows others to use what you
 -- wrote in their libraries.
-import qualified Example
 
 main :: IO ()
-main = Example.main
+main = return ()
