3ad41fdec78a01fd50e6b9003c8d68412aa2f321
YoEight <yo.eight@gmail.com>
2017-07-14 00:14:52 +0200

    Add more runtime utilities

--- lambda-node/library/Lambda/Node/Prelude.hs
+++ lambda-node/library/Lambda/Node/Prelude.hs
@@ -13,9 +13,18 @@ module Lambda.Node.Prelude
   ( module ClassyPrelude
   , module Control.Monad.Logger
   , module Control.Monad.Logger.CallStack
+  , module Data.String.Interpolate.IsString
+  , module System.Clock
+  , clockTime
   ) where
 
 --------------------------------------------------------------------------------
 import ClassyPrelude
 import Control.Monad.Logger hiding (logDebug, logInfo, logWarn, logError)
 import Control.Monad.Logger.CallStack
+import Data.String.Interpolate.IsString
+import System.Clock
+
+--------------------------------------------------------------------------------
+clockTime :: MonadIO m => m TimeSpec
+clockTime = liftIO $ getTime Monotonic
